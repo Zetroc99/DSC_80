1c2d0e9b18cfca30453ae62e97f62a7f1e7771a7
Zetroc99 <49223290+Zetroc99@users.noreply.github.com>
2021-04-20 01:12:26 -0700

    Up to 5, finish 6
    testing reqeired

---
+++ labs/03/lab03.py
@@ -0,0 +1,374 @@
+import os
+
+import pandas as pd
+import numpy as np
+
+
+# ---------------------------------------------------------------------
+# Question # 1
+# ---------------------------------------------------------------------
+
+def car_null_hypoth():
+    """
+    Returns a list of valid null hypotheses.
+    
+    :Example:
+    >>> set(car_null_hypoth()) <= set(range(1,11))
+    True
+    """
+    return [3, 7]
+
+
+def car_alt_hypoth():
+    """
+    Returns a list of valid alternative hypotheses.
+    
+    :Example:
+    >>> set(car_alt_hypoth()) <= set(range(1,11))
+    True
+    """
+    return [1, 2, 8]
+
+
+def car_test_stat():
+    """
+    Returns a list of valid test statistics.
+    
+    :Example:
+    >>> set(car_test_stat()) <= set(range(1,5))
+    True
+    """
+    return [2, 4]
+
+
+def car_p_value():
+    """
+    Returns an integer corresponding to the correct explanation.
+    
+    :Example:
+    >>> car_p_value() in [1,2,3,4,5]
+    True
+    """
+    return 2
+
+
+# ---------------------------------------------------------------------
+# Question #2
+# ---------------------------------------------------------------------
+
+def clean_apps(df):
+    """
+    >>> fp = os.path.join('data', 'googleplaystore.csv')
+    >>> df = pd.read_csv(fp)
+    >>> cleaned = clean_apps(df)
+    >>> len(cleaned) == len(df)
+    True
+    """
+    df_copy = df.copy(deep=True)
+
+    def strip_size(string):
+        if 'M' in string:
+            size_str = string.strip('M')
+            size = float(size_str) * 1000
+        elif 'k' in string:
+            size_str = string.strip('k')
+            size = float(size_str)
+        return size
+
+    def install_change(app):
+        return int(app.strip('+').replace(',', ''))
+
+    def price_change(price):
+        return float(price.strip('$'))
+
+    def time_change(time):
+        return int(time[:4])
+
+    (df['Type'] == 'Free').astype(int)
+
+    df_copy['Size'] = df_copy['Size'].apply(strip_size)
+    df_copy['Installs'] = df_copy['Installs'].apply(install_change)
+    df_copy['Type'] = (df_copy['Type'] == 'Free').astype(int)
+    df_copy['Price'] = df_copy['Price'].apply(price_change)
+    df_copy['Last Updated'] = pd.to_datetime(df_copy['Last Updated']) \
+        .astype(str).apply(time_change)
+
+    return df_copy
+
+
+def store_info(cleaned):
+    """
+    >>> fp = os.path.join('data', 'googleplaystore.csv')
+    >>> df = pd.read_csv(fp)
+    >>> cleaned = clean_apps(df)
+    >>> info = store_info(cleaned)
+    >>> len(info)
+    4
+    >>> info[2] in cleaned.Category.unique()
+    True
+    """
+
+    return [2018, 'Adults only 18+', 'FINANCE', 'DATING']
+
+
+# ---------------------------------------------------------------------
+# Question 3
+# ---------------------------------------------------------------------
+
+def std_reviews_by_app_cat(cleaned):
+    """
+    >>> fp = os.path.join('data', 'googleplaystore.csv')
+    >>> play = pd.read_csv(fp)
+    >>> clean_play = clean_apps(play)
+    >>> out = std_reviews_by_app_cat(clean_play)
+    >>> set(out.columns) == set(['Category', 'Reviews'])
+    True
+    >>> np.all(abs(out.select_dtypes(include='number').mean()) < 10**-7)  # standard units should average to 0!
+    True
+    """
+    norm_review = \
+        cleaned.groupby('Category').transform(
+            lambda x: (x - x.mean()) / x.std())[
+            'Reviews']
+    return pd.DataFrame({'Category': cleaned['Category'],
+                         'Reviews': norm_review})
+
+
+def su_and_spread():
+    """
+    >>> out = su_and_spread()
+    >>> len(out) == 2
+    True
+    >>> out[0].lower() in ['medical', 'family', 'equal']
+    True
+    >>> out[1] in ['ART_AND_DESIGN', 'AUTO_AND_VEHICLES', 'BEAUTY',\
+       'BOOKS_AND_REFERENCE', 'BUSINESS', 'COMICS', 'COMMUNICATION',\
+       'DATING', 'EDUCATION', 'ENTERTAINMENT', 'EVENTS', 'FINANCE',\
+       'FOOD_AND_DRINK', 'HEALTH_AND_FITNESS', 'HOUSE_AND_HOME',\
+       'LIBRARIES_AND_DEMO', 'LIFESTYLE', 'GAME', 'FAMILY', 'MEDICAL',\
+       'SOCIAL', 'SHOPPING', 'PHOTOGRAPHY', 'SPORTS', 'TRAVEL_AND_LOCAL',\
+       'TOOLS', 'PERSONALIZATION', 'PRODUCTIVITY', 'PARENTING', 'WEATHER',\
+       'VIDEO_PLAYERS', 'NEWS_AND_MAGAZINES', 'MAPS_AND_NAVIGATION']
+    True
+    """
+    return ['equal', 'GAME']
+
+
+# ---------------------------------------------------------------------
+# Question #4
+# ---------------------------------------------------------------------
+
+
+def read_survey(dirname):
+    """
+    read_survey combines all the survey*.csv files into a singular DataFrame
+    :param dirname: directory name where the survey*.csv files are
+    :returns: a DataFrame containing the combined survey data
+    :Example:
+    >>> dirname = os.path.join('data', 'responses')
+    >>> out = read_survey(dirname)
+    >>> isinstance(out, pd.DataFrame)
+    True
+    >>> len(out)
+    5000
+    >>> read_survey('nonexistentfile') # doctest: +ELLIPSIS
+    Traceback (most recent call last):
+    ...
+    FileNotFoundError: ... 'nonexistentfile'
+    """
+    col_names = ['first name', 'last name', 'current company', 'job title',
+                 'email', 'university']
+    df_load = pd.DataFrame(columns=col_names)
+    surveys = os.listdir(dirname)
+
+    for survey in surveys:
+        df = pd.read_csv(dirname + '\\' + survey)
+        df = df.sort_index(axis=1)
+        df.columns = np.sort(col_names)
+        df_load = pd.concat([df_load, df], ignore_index=True)
+
+    return df_load.fillna(df_load.dtypes.replace({'O': 'NULL'}), inplace=True)
+
+
+def com_stats(df):
+    """
+    com_stats 
+    :param df: a DataFrame containing the combined survey data
+    :returns: a hardcoded list of answers to the problems in the notebook
+    :Example:
+    >>> dirname = os.path.join('data', 'responses')
+    >>> df = read_survey(dirname)
+    >>> out = com_stats(df)
+    >>> len(out)
+    4
+    >>> isinstance(out[0], int)
+    True
+    >>> isinstance(out[2], str)
+    True
+    """
+
+    return [5, 259, 'Business Systems Development Analyst', 369]
+
+
+# ---------------------------------------------------------------------
+# Question #5
+# ---------------------------------------------------------------------
+
+def combine_surveys(dirname):
+    """
+    combine_surveys takes in a directory path 
+    (containing files favorite*.csv) and combines 
+    all of the survey data into one DataFrame, 
+    indexed by student ID (a value 1 - 1000).
+
+    :Example:
+    >>> dirname = os.path.join('data', 'extra-credit-surveys')
+    >>> out = combine_surveys(dirname)
+    >>> isinstance(out, pd.DataFrame)
+    True
+    >>> out.shape
+    (1000, 6)
+    >>> combine_surveys('nonexistentfile') # doctest: +ELLIPSIS
+    Traceback (most recent call last):
+    ...
+    FileNotFoundError: ... 'nonexistentfile'
+    """
+    df_load = pd.DataFrame({'id': [i for i in range(1, 1001)]}).set_index('id')
+    surveys = os.listdir(dirname)
+
+    for survey in surveys:
+        df = pd.read_csv(dirname + '\\' + survey)
+        df_load[df.columns[1]] = df.iloc[:, 1:]
+    return df_load
+
+
+def check_credit(df):
+    """
+    check_credit takes in a DataFrame with the 
+    combined survey data and outputs a DataFrame 
+    of the names of students and how many extra credit 
+    points they would receive, indexed by their ID (a value 1-1000)
+
+    :Example:
+    >>> dirname = os.path.join('data', 'extra-credit-surveys')
+    >>> df = combine_surveys(dirname)
+    >>> out = check_credit(df)
+    >>> out.shape
+    (1000, 2)
+    """
+    EC = pd.DataFrame({'id': [i for i in range(1, 1001)]}).set_index('id')
+    EC['name'] = df['name']
+
+    plus_5 = df.iloc[:, 1:].notnull().sum(axis=1) / (df.shape[1] - 1) > 0.75
+    df['EC'] = np.where(plus_5, 5, 0)
+
+    if np.any((df.notnull().sum() / df.shape[0]) > .9):
+        EC['EC'] = df[['EC']].add(1)
+
+    return EC
+
+
+# ---------------------------------------------------------------------
+# Question # 6
+# ---------------------------------------------------------------------
+
+
+def most_popular_procedure(pets, procedure_history):
+    """
+    What is the most popular Procedure Type for all of the pets we have in our `pets` dataset?
+​
+    :Example:
+    >>> pets_fp = os.path.join('data', 'pets', 'Pets.csv')
+    >>> procedure_history_fp = os.path.join('data', 'pets', 'ProceduresHistory.csv')
+    >>> pets = pd.read_csv(pets_fp)
+    >>> procedure_history = pd.read_csv(procedure_history_fp)
+    >>> out = most_popular_procedure(pets, procedure_history)
+    >>> isinstance(out,str)
+    True
+    """
+
+    return ...
+
+
+def pet_name_by_owner(owners, pets):
+    """
+    pet names by owner
+
+    :Example:
+    >>> owners_fp = os.path.join('data', 'pets', 'Owners.csv')
+    >>> pets_fp = os.path.join('data', 'pets', 'Pets.csv')
+    >>> owners = pd.read_csv(owners_fp)
+    >>> pets = pd.read_csv(pets_fp)
+    >>> out = pet_name_by_owner(owners, pets)
+    >>> len(out) == len(owners)
+    True
+    >>> 'Sarah' in out.index
+    True
+    >>> 'Cookie' in out.values
+    True
+    """
+
+    return ...
+
+
+def total_cost_per_city(owners, pets, procedure_history, procedure_detail):
+    """
+    total cost per city
+​
+    :Example:
+    >>> owners_fp = os.path.join('data', 'pets', 'Owners.csv')
+    >>> pets_fp = os.path.join('data', 'pets', 'Pets.csv')
+    >>> procedure_detail_fp = os.path.join('data', 'pets', 'ProceduresDetails.csv')
+    >>> procedure_history_fp = os.path.join('data', 'pets', 'ProceduresHistory.csv')
+    >>> owners = pd.read_csv(owners_fp)
+    >>> pets = pd.read_csv(pets_fp)
+    >>> procedure_detail = pd.read_csv(procedure_detail_fp)
+    >>> procedure_history = pd.read_csv(procedure_history_fp)
+    >>> out = total_cost_per_city(owners, pets, procedure_history, procedure_detail)
+    >>> set(out.index) <= set(owners['City'])
+    True
+    """
+
+    return ...
+
+
+# ---------------------------------------------------------------------
+# DO NOT TOUCH BELOW THIS LINE
+# IT'S FOR YOUR OWN BENEFIT!
+# ---------------------------------------------------------------------
+
+
+# Graded functions names! DO NOT CHANGE!
+# This dictionary provides your doctests with
+# a check that all of the questions being graded
+# exist in your code!
+
+
+GRADED_FUNCTIONS = {
+    'q01': [
+        'car_null_hypoth', 'car_alt_hypoth',
+        'car_test_stat', 'car_p_value'
+    ],
+    'q02': ['clean_apps', 'store_info'],
+    'q03': ['std_reviews_by_app_cat', 'su_and_spread'],
+    'q04': ['read_survey', 'com_stats'],
+    'q05': ['combine_surveys', 'check_credit'],
+    'q06': ['most_popular_procedure', 'pet_name_by_owner',
+            'total_cost_per_city']
+}
+
+
+def check_for_graded_elements():
+    """
+    >>> check_for_graded_elements()
+    True
+    """
+
+    for q, elts in GRADED_FUNCTIONS.items():
+        for elt in elts:
+            if elt not in globals():
+                stmt = "YOU CHANGED A QUESTION THAT SHOULDN'T CHANGE! \
+                In %s, part %s is missing" % (q, elt)
+                raise Exception(stmt)
+
+    return True
