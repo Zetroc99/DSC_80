9077c79a61c9ba7fb53891cfac2cf6a00cc145b1
Zetroc99 <49223290+Zetroc99@users.noreply.github.com>
2021-06-02 12:20:39 -0700

    lab09 complete

--- labs/09/lab09.py
+++ labs/09/lab09.py
@@ -3,17 +3,18 @@ import numpy as np
 import seaborn as sns
 import os
 
-
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import FunctionTransformer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import Binarizer
 
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsRegressor
-from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import GridSearchCV
 
 
 # ---------------------------------------------------------------------
@@ -22,9 +23,9 @@ from sklearn.model_selection import train_test_split
 
 
 def simple_pipeline(data):
-    '''
-    simple_pipeline takes in a dataframe like data and returns a tuple 
-    consisting of the pipeline and the predictions your model makes 
+    """
+    simple_pipeline takes in a dataframe like data and returns a tuple
+    consisting of the pipeline and the predictions your model makes
     on data (as trained on data).
 
     :Example:
@@ -39,18 +40,21 @@ def simple_pipeline(data):
     True
     >>> preds.shape[0] == data.shape[0]
     True
-    '''
-    
-    return ...
+    """
+    pl = Pipeline(
+        [('logScale', FunctionTransformer(np.log)), ('lr', LinearRegression())])
+
+    return pl.fit(data[['c2']], data['y']), pl.predict(data[['c2']])
+
 
 # ---------------------------------------------------------------------
 # Question # 2
 # ---------------------------------------------------------------------
 
 def multi_type_pipeline(data):
-    '''
-    multi_type_pipeline that takes in a dataframe like data and 
-    returns a tuple consisting of the pipeline and the predictions 
+    """
+    multi_type_pipeline that takes in a dataframe like data and
+    returns a tuple consisting of the pipeline and the predictions
     your model makes on data (as trained on data).
 
     :Example:
@@ -65,9 +69,23 @@ def multi_type_pipeline(data):
     True
     >>> data.shape[0] == preds.shape[0]
     True
-    '''
+    """
+    num_feat = ['c2']
+    cat_feat = ['group']
 
-    return ...
+    pp = ColumnTransformer(
+        transformers=[
+            ('num', FunctionTransformer(np.log), num_feat),
+            ('cat', OneHotEncoder(), cat_feat)
+        ], remainder='passthrough')
+
+    pl = Pipeline(
+        steps=[('preprocess', pp), ('regression', LinearRegression())])
+    pl.fit(data.drop('y', axis=1), data.y)
+
+    return pl.fit(data.drop('y', axis=1), data.y), pl.predict(data.drop(
+        'y', axis=1))
+
 
 # ---------------------------------------------------------------------
 # Question # 3
@@ -77,7 +95,7 @@ from sklearn.base import BaseEstimator, TransformerMixin
 
 
 class StdScalerByGroup(BaseEstimator, TransformerMixin):
-    
+
     def __init__(self):
         pass
 
@@ -92,9 +110,14 @@ class StdScalerByGroup(BaseEstimator, TransformerMixin):
         """
         # X may not be a pandas dataframe (e.g. a np.array)
         df = pd.DataFrame(X)
-        
-        # A dictionary of means/standard-deviations for each column, for each group.
-        self.grps_ = ...
+        df_groups = df[df.columns[0]].sort_values().unique()
+
+        df_group_mean = df.groupby(df.columns[0]).mean()
+        df_group_std = df.groupby(df.columns[0]).std()
+
+        # A dictionary of means/standard-deviations for each column,
+        # for each group.
+        self.grps_ = df.groupby(df.columns[0]).agg(['mean', 'std']).to_dict()
 
         return self
 
@@ -114,17 +137,16 @@ class StdScalerByGroup(BaseEstimator, TransformerMixin):
         try:
             getattr(self, "grps_")
         except AttributeError:
-            raise RuntimeError("You must fit the transformer before tranforming the data!")
-        
+            raise RuntimeError(
+                "You must fit the transformer before transforming the data!")
 
         # Define a helper function here?
-
+        def transform_helper(x):
+            return (x-x.mean()) / x.std()
         # X may not be a dataframe (e.g. np.array)
-
         df = pd.DataFrame(X)
-        
-        return ...
 
+        return df.groupby(df.columns[0]).transform(transform_helper)
 
 # ---------------------------------------------------------------------
 # Question # 4
@@ -140,7 +162,7 @@ def eval_toy_model():
     True
     """
 
-    return ...
+    return [0.3956, 0.5733, 0.5730]
 
 
 # ---------------------------------------------------------------------
@@ -159,8 +181,26 @@ def tree_reg_perf(galton):
     >>> out['train_err'].iloc[-1] < out['test_err'].iloc[-1]
     True
     """
+    X = galton.drop('childHeight', axis=1)
+    y = galton.childHeight
 
-    return ...
+    rmse_train = []
+    rmse_test = []
+    for depth in range(1, 21):
+        X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                            test_size=0.30)
+        treeReg = DecisionTreeRegressor(max_depth=depth).fit(X_train, y_train)
+        pred_train = treeReg.predict(X_train)
+        pred_test = treeReg.predict(X_test)
+
+        err_train = (np.sum((y_train - pred_train) ** 2) / len(y_train)) ** 0.5
+        rmse_train.append(err_train)
+
+        err_test = (np.sum((y_test - pred_test) ** 2) / len(y_test)) ** 0.5
+        rmse_test.append(err_test)
+
+    return pd.DataFrame(index=np.arange(1, 21), data={'train_err': rmse_train,
+                                                      'test_err': rmse_test})
 
 
 def knn_reg_perf(galton):
@@ -172,8 +212,27 @@ def knn_reg_perf(galton):
     >>> out.columns.tolist() == ['train_err', 'test_err']
     True
     """
+    X = galton.drop('childHeight', axis=1)
+    y = galton.childHeight
 
-    return ...
+    rmse_train = []
+    rmse_test = []
+    for n in range(1, 21):
+        X_train, X_test, y_train, y_test = train_test_split(X,
+                                                            y, test_size=0.30)
+        knnReg = KNeighborsRegressor(n_neighbors=n).fit(X_train, y_train)
+        pred_train = knnReg.predict(X_train)
+        pred_test = knnReg.predict(X_test)
+
+        err_train = (np.sum((y_train - pred_train) ** 2) / len(y_train)) ** 0.5
+        rmse_train.append(err_train)
+
+        err_test = (np.sum((y_test - pred_test) ** 2) / len(y_test)) ** 0.5
+        rmse_test.append(err_test)
+
+    return pd.DataFrame(index=np.arange(1, 21), data={'train_err': rmse_train,
+                                                      'test_err': rmse_test})
+
 
 # ---------------------------------------------------------------------
 # Question # 6
@@ -194,8 +253,29 @@ def titanic_model(titanic):
     >>> ((preds == 0)|(preds == 1)).all()
     True
     """
+    titanic = titanic.copy()
+    titanic['Name'] = titanic.Name.map(lambda x: x.split('.')[0].strip())
+    titanic['Name'] = titanic['Name'].apply(
+        lambda x: 1 if 'Mrs' in x or 'Miss' in x else 0)
 
-    return ...
+    X = titanic.drop('Survived', axis=1)
+    y = titanic.Survived
+
+    class_group = ['Pclass', 'Age', 'Fare']
+    cat_feat = list(titanic.select_dtypes(include='object').columns)
+
+    ct = ColumnTransformer([
+        ('class', StdScalerByGroup(), class_group),
+        ('categories', OneHotEncoder(), cat_feat)
+    ])
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30)
+    params = {'KNN__n_neighbors': [2, 3, 4, 5, 6, 7, 8, 9, 10]}
+    clf = Pipeline(
+        steps=[('colTrans', ct), ('grid', KNeighborsClassifier(n_neighbors=9))])
+
+    return clf.fit(X_train, y_train)
+
 
 # ---------------------------------------------------------------------
 # Question # 7
@@ -219,8 +299,11 @@ def json_reader(file, iterations):
     >>> len(labels) == len(reviews)
     True
     """
+    reviews = pd.read_json(file, lines=True).head(iterations)
+    reviewsText = reviews.reviewText.tolist()
+    rating = reviews.overall.tolist()
 
-    return ...
+    return reviewsText, rating
 
 
 def create_classifier_multi(X, y):
@@ -232,7 +315,7 @@ def create_classifier_multi(X, y):
     >>> isinstance(trial, Pipeline)
     True
     """
-    
+
     return ...
 
 
@@ -244,7 +327,7 @@ def to_binary(labels):
     >>> print(lst)
     [0, 0, 0, 1, 1]
     """
-    
+
     return ...
 
 
@@ -288,12 +371,12 @@ def check_for_graded_elements():
     >>> check_for_graded_elements()
     True
     """
-    
+
     for q, elts in GRADED_FUNCTIONS.items():
         for elt in elts:
             if elt not in globals():
                 stmt = "YOU CHANGED A QUESTION THAT SHOULDN'T CHANGE! \
-                In %s, part %s is missing" %(q, elt)
+                In %s, part %s is missing" % (q, elt)
                 raise Exception(stmt)
 
     return True
